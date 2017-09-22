bd6d1bb6400eec66e3b51b3cabe7c0fc9c79899a
Neil Agarwal <regalstreak@gmail.com>
2017-09-22 18:39:33 +0530

    app: Do cardviews

--- app/src/main/java/me/regalstreak/wallpapers/WallpaperStuff.java
+++ app/src/main/java/me/regalstreak/wallpapers/WallpaperStuff.java
@@ -1,14 +1,17 @@
 package me.regalstreak.wallpapers;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class WallpaperStuff extends AppCompatActivity {
 
+    TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wallpaper_stuff);
+        title = (TextView) findViewById(R.id.wallpaperStuff_Title);
     }
 }
