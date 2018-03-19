0ee095c0a74ea10a8d8efe9c216af6eda329263e
Neil Agarwal <regalstreak@gmail.com>
2018-03-19 21:00:17 +0530

    app: Remove comments

--- app/src/main/java/me/regalstreak/wallpapers/WallPreview.java
+++ app/src/main/java/me/regalstreak/wallpapers/WallPreview.java
@@ -25,11 +25,9 @@ public class WallPreview extends AppCompatActivity {
     public void fullScreen() {
 
         // BEGIN_INCLUDE (get_current_ui_flags)
-        // The UI options currently enabled are represented by a bitfield.
-        // getSystemUiVisibility() gives us that bitfield.
         int uiOptions = getWindow().getDecorView().getSystemUiVisibility();
         int newUiOptions = uiOptions;
-        // END_INCLUDE (get_current_ui_flags)
+
         // BEGIN_INCLUDE (toggle_ui_flags)
         boolean isImmersiveModeEnabled =
                 ((uiOptions | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) == uiOptions);
@@ -50,18 +48,10 @@ public class WallPreview extends AppCompatActivity {
         }
 
         // Immersive mode: Backward compatible to KitKat.
-        // Note that this flag doesn't do anything by itself, it only augments the behavior
-        // of HIDE_NAVIGATION and FLAG_FULLSCREEN.  For the purposes of this sample
-        // all three flags are being toggled together.
-        // Note that there are two immersive mode UI flags, one of which is referred to as "sticky".
-        // Sticky immersive mode differs in that it makes the navigation and status bars
-        // semi-transparent, and the UI flag does not get cleared when the user interacts with
-        // the screen.
         if (Build.VERSION.SDK_INT >= 18) {
             newUiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
         }
 
         getWindow().getDecorView().setSystemUiVisibility(newUiOptions);
-        //END_INCLUDE (set_ui_flags)
     }
 }
